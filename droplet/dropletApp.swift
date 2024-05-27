8ec7b1221fa4b19badb7487c7378a29492a3273f
Anna <119071299+heyanja@users.noreply.github.com>
2024-05-27 10:35:18 -0400

    Initial Commit

---
+++ droplet/dropletApp.swift
@@ -0,0 +1,17 @@
+//
+//  dropletApp.swift
+//  droplet
+//
+//  Created by Anna Fadieieva on 2024-05-27.
+//
+
+import SwiftUI
+
+@main
+struct dropletApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
